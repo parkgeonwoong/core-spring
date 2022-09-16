fd11b7d50dfc64b1a93e695bdaaf6962ce02a291
parkgeonwoong <hk77kh@gmail.com>
2022-09-16 16:34:58 +0900

    Refactor: AppConfig를 역할안에 구현 한눈에 보이게

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -5,7 +5,9 @@ package hello.core;
  * AppConfig(외부)에서 어떤 구현 객체를 주입할지 정한다
  */
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -14,15 +16,32 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    /**
+     * @desc: 리팩토링 -> 중복 제거 및 한눈에 들어오게
+     *  역할이 메소드명 구현 = new ...
+     */
+
+//    public MemberService memberService() {
+//        return new MemberServiceImpl(new MemoryMemberRepository());
+//    }
+
     // 멤버 서비스
     public MemberService memberService() {
         // 생성자 주입
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     // 주문 서비스
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
