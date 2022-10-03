07d9f3fb677880af877a5c51de8e595b0d83bfe6
parkgeonwoong <hk77kh@gmail.com>
2022-10-03 18:51:01 +0900

    Test : ApplicationContext XML로 해보기

---
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,22 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+
+    /**
+     * @desc : XML 구현 해보기  (지금까지 자바코드로 구현 = AppConfig)
+     * 여기는 appConfig.xml로 가져온것
+     */
+
+    @Test
+    void XmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
