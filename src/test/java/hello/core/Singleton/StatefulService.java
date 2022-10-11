9790198949a585c8ec38b1d84c6df4795abdd77f
parkgeonwoong <hk77kh@gmail.com>
2022-10-11 15:54:56 +0900

    Feat : Statefuless

---
+++ src/test/java/hello/core/Singleton/StatefulService.java
@@ -0,0 +1,17 @@
+package hello.core.Singleton;
+
+public class StatefulService {
+
+    // private int price;
+
+    public int order(String name, int price) {
+        System.out.println("name = " + name + " price : " + price);
+        // this.price = price; // 이게 문제
+        return price;
+    }
+
+//    public int getPrice() {
+//        return price;
+//    }
+
+}
