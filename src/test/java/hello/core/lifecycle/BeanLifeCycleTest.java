f0cd6818e56bbb17bea6c73be7f8e8b5d9373b48
parkgeonwoong <hk77kh@gmail.com>
2022-10-13 17:02:13 +0900

    Feat : 빈 생명주기

---
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,37 @@
+package hello.core.lifecycle;
+/**
+ * @desc : LifeCycle Test
+ */
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+
+        client.setUrl("http://Test.dev");
+        client.connect();
+
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");        // 객체 생성 -> 의존 관계 주입
+            return networkClient;
+        }
+    }
+}
