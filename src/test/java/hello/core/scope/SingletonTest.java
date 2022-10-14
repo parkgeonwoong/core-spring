82d31433b131bc90c4b3df491a582481987587de
parkgeonwoong <hk77kh@gmail.com>
2022-10-14 15:04:11 +0900

    Test : 싱글톤, 프로토타입 스코프

---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,50 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * @desc : 프로토타입 스코프 테스트
+ */
+
+
+public class SingletonTest {
+
+    @Test
+    void SingletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+
+        assertThat(singletonBean1).isSameAs(singletonBean1);
+
+        ac.close();
+    }
+
+    // 스코프
+    @Scope("singleton")
+    static class SingletonBean {
+
+        // 빈 생명주기 init
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
